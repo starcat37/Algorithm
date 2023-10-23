21444b3911f8f5915ed7c42b1efd65b25a4adfe1
starcat37 <starcat37@korea.ac.kr>
2023-10-23 11:44:41 +0900

    231023 문제풀이

---
+++ BOJ/Bronze/24267.py
@@ -0,0 +1,20 @@
+# 24267
+
+n = int(input())
+print(int(n*(n-1)*(n-2)/6))
+print(3)
+
+
+# 조합 공식 이용, r = 3
+
+# 시간 초과
+# N = int(input())
+# cnt = 0
+
+# for i in range(1, N-1):
+#   for j in range(i+1, N):
+#     for k in range(j+1, N+1):
+#       cnt += 1
+
+# print(cnt)
+# print(3)
